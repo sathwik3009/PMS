43607e8d9ec90e7e367c80074f0203c617e71007
Ashmn07 <56076713+Ashmn07@users.noreply.github.com>
2021-05-15 12:57:08 +0530

    Update newmed.php

--- newmed.php
+++ newmed.php
@@ -1,7 +1,4 @@
 <?php
-// if(isset($_GET['cid'])){
-//   $id=$_GET['cid'];
-// }
 session_start();
 $servername = "localhost:3306";
 $username = "root";
@@ -10,7 +7,7 @@ $dbname = "pms";
 $verify_err_mess="";
 $correct_mess="";
 
-$sessEmail = $_SESSION["user_email"];
+$id = $_SESSION["user_id"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -20,31 +17,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   $medicineName = $_POST['mn'];
 	$quantity = $_POST['q'];
 	$ppq = $_POST['ppq'];
-  $tableId="";
-	// $password = $_POST['pwd'];
-  // $email = $_POST['em'];
-
-//   echo '<h1>'.$sessEmail.'</h1>';
-
-  $query1 = "SELECT * FROM customer WHERE Customer_Email='$sessEmail'";
-  $result = mysqli_query($conn,$query1);
-	$count  = mysqli_num_rows($result);
-  if($count==0){
-    $verify_err_mess="Invalid Email / Password";
+  $query = "INSERT INTO seller_medicines(S_ID,MName,MQuantity,PPQ)
+    VALUES ('$id','$medicineName','$quantity','$ppq')";
+    if ($conn->query($query) === TRUE) {
+      $correct_mess="Medicine Details have been Added";
+  } else {
+    $verify_err_mess="Error ! Could not Add to table.";
   }
-  else{
-    while($row = $result->fetch_assoc()) {
-      $tableId =  $row["Customer_ID"];
-    }
-    $query2 = "INSERT INTO seller_medicines(S_ID,MName,MQuantity,PPQ)
-     VALUES ('$tableId','$medicineName','$quantity','$ppq')";
-      if ($conn->query($query2) === TRUE) {
-        $correct_mess="Medicine Details have been Added";
-    } else {
-      $verify_err_mess="Error ! Could not Add to table.";
-    }
   }
-}
 ?>
 <!DOCTYPE html>
 <html>
@@ -63,6 +43,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     .card{
         border-radius: 10px;
     }
+    .ui.fluid.button{
+      margin:3%;
+      margin-left:auto;
+      margin-right:auto;
+      width: 20% !important;;
+    }
 </style>
 </head>
 <body>
@@ -76,7 +62,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <a href="newmed.php" class="ui item">
         <h4>New Stock</h4>
         </a>
-        <a href="/" class="ui item">
+        <a href="editstock.php" class="ui item">
         <h4>Edit Stock</h4>
         </a>
         <a href="/" class="ui item">
@@ -121,7 +107,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <label for="ppq"><b>Price Per Quantity</b></label>
             <input type="number" step="0.01" placeholder="Enter Price Per Quantity" name="ppq" required>
           </div>
-          <button type="submit" class="btn">ADD</button>
+          <button type="submit" class="ui fluid button btn">ADD</button>
         </form>
         </div>
       </div>
